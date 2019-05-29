e0ef33b88c4fe69b290ef48387702daf47f58bc5
rainavyas <sayvaniar@gmail.com>
2019-05-29 10:02:39 +0100

    Created LBT scheme optimiser

---
+++ my_LBT_optimiser.m
@@ -0,0 +1,8 @@
+function my_LBT_optimiser()
+
+% This function finds the parameter settings for the LBT (in JPEG scheme)
+% that gives the highest SSIM relative to the original image, whilst
+% getting number of compression bits to be less than 5kbytes
+%Parameters: N, s, qstep, M, opthuff = true/false
+
+return
