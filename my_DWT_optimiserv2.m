20b500ebd8a79119314c11369ec3923ff5e3e335
rainavyas <sayvaniar@gmail.com>
2019-05-31 15:06:55 +0100

    as file says

--- my_DWT_optimiserv2.m
+++ my_DWT_optimiserv2.m
@@ -9,19 +9,15 @@ function my_DWT_optimiserv2(X)
 
 MAX_size = 40960;
 X = double(X);
-X = X -128;
-N_list = [4,8,16,32];
+%X = X -128;
+N_list = [4, 8,16,32];
 dc_bits = 8;
 
 for i = 1:length(N_list)
     
-    % Give an upperbound on number of bits to beat
-    % best = [bits, s, M, SSIM]
+    % best = [total_bits, n, M, SSIM]
     best = [MAX_size * 3, -1, -1, -1];
     
-    n = best(2);
-    M = best(3);
-    total_bits = best(1);
     N = N_list(i);
     
     %Loop through M and n to find smallest number of bits
@@ -29,10 +25,12 @@ for i = 1:length(N_list)
         for j = 0: (log2(256)-log2(N))
             M = N *(2^j);
             %Initisalise quantisation step
-            qstep = 25;
+            qstep = 5;
+            total_bits = MAX_size * 3;
             while (total_bits > MAX_size && qstep < 256)
+                
                 try
-                   [vlc bits huffval] = DWTenc(X, qstep, N_list(i), n, M, 0, dc_bits);
+                   [vlc bits huffval] = DWTenc(X, qstep, N, n, M, 0, dc_bits);
                    total_bits = sum(vlc(:,2));
                 catch ME
                     disp("hey I caught an error")
@@ -42,19 +40,25 @@ for i = 1:length(N_list)
             Zp = DWTdec(vlc, qstep, n, N, M, bits, huffval, dc_bits);
             SSIM = ssim(Zp, X);
                
-            if SSIM < best(4)
+            if SSIM > best(4)
                 best = [total_bits, n, M, SSIM];
             end
         end
     end
     
     qstep = qstep -1;
+    total_bits = best(1);
+    n = best(2);
+    M = best(3);
+    SSIM = best(4);
       
     % Display results
     disp("N: "+ N+ ", bits: "+ total_bits+ ", n: "+ n+ ", M: "+M+ ", ssim: "+SSIM + ", step: "+ qstep);
     
-    figure(N);
-    draw(Zp);
+    %figure(N);
+    %[vlc bits huffval] = DWTenc(X, qstep, N, n, M, 0, dc_bits);
+    %Zp = DWTdec(vlc, qstep, n, N, M, bits, huffval, dc_bits);
+    %draw(Zp);
 end
     
     
