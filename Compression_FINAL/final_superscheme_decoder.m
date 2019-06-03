2bc889076ac3ef9e1464cfdf0a6bcad5ad8e08c7
A. Liusie <al826@tw705.eng.cam.ac.uk>
2019-06-03 11:02:41 +0100

    git push

--- Compression_FINAL/final_superscheme_decoder.m
+++ Compression_FINAL/final_superscheme_decoder.m
@@ -1,7 +1,7 @@
 function Z = final_superscheme_decoder(scheme, vlc, bits, huffval, N, step, s_n, rms)
             
     if scheme == 1    
-       Z = jpegdec(vlc,step,N,N,bits,huffval,8);
+       Z = jpegdec(vlc,step,N,N,bits,huffval,8) + 128;
        display("DCT encoding used")
        display("N/M : " + N + ", step : " + step)
        display("RMS error : " + rms + ", bits : " + (sum(vlc(:,2))+1434)) 
@@ -9,7 +9,7 @@ function Z = final_superscheme_decoder(scheme, vlc, bits, huffval, N, step, s_n,
     end 
     
     if scheme == 2
-       Z = LBTdec(vlc,step,s_n,N,N,bits,huffval,8);
+       Z = LBTdec(vlc,step,s_n,N,N,bits,huffval,8) + 128;
        display("LBT encoding used")
        display("N/M : " + N + ", step : " + step + ", s: " + s_n)
        display("RMS error : " + rms + ", bits : " + (sum(vlc(:,2))+1434)) 
